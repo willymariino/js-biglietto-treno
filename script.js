48ef23d14f15d4547db6c37090cf1d461dcde816
willymariino <wg.mariino@gmail.com>
2025-02-24 16:48:59 +0100

    added part with age based discounts

---
+++ script.js
@@ -0,0 +1,14 @@
+// chiedere all'utente le informazioni necessarie
+const km = parseInt(prompt("quanti chilometri vuoi percorrere?"));
+const age = parseInt(prompt("quanti anni hai?"));
+
+// calcolare il prezzo del biglietto
+const price = km * 0.21;
+let discount = 0;
+
+//applicare sconti in base all'età
+if (age < 18) {
+    discount = price * 0.2;
+} else if (age > 65) {
+    discount = price * 0.4;
+}
